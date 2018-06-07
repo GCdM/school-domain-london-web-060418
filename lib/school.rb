2d170da3c2c098d7b5ecb4f7a8a6628539dba359
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-06-07 08:16:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  attr_accessor :roster
+  
+  def initialize(roster)
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    if @roster.include?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = [student]
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster = Hash[@roster.sort_by {|grade, students| grade}]
+    @roster.each do |grade, students|
+      students.sort!
+    end
+  end
+end
